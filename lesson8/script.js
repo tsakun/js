1b5b04c969728d830cea941b512838a5912ff730
Serg <tsakun@gmail.com>
2020-02-07 09:13:29 +0200

    9 lesson

--- lesson8/script.js
+++ lesson8/script.js
@@ -22,6 +22,33 @@ for (let i=0; i<2; i++) {
     }
 }
 
+
+// let i=0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце","");
+//         b = prompt("Во сколько обойдется?","");
+//     i++;
+//     if ((typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = +b;
+//     } else {
+//         i--; 
+//     }
+// }while(i<2)
+
+// let i=0;
+// while (i<2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце","");
+//         b = prompt("Во сколько обойдется?","");
+//     i++;
+//     if ((typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = +b;
+//     } else {
+//         i--; 
+//     }
+// }
+
 appData.moneyPerDay = appData.budget / 30;
 
 alert("Ваш буджет на 1 день = " + appData.moneyPerDay);
