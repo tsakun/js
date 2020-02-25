7d742b291567dce5b4dfabb31e08d763656bdd73
Serg <tsakun@gmail.com>
2020-02-25 20:18:49 +0200

    Modal window

--- exercise9/yoga/js/script.js
+++ exercise9/yoga/js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
     
+    
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -81,4 +82,22 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    //Modal window
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+    
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    })
 });
